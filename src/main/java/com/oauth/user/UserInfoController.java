be91456cbf1e6866e099ade66d452d45d7057c86
chenlizan <clz_318@live.com>
2019-07-23 23:22:35 +0800

    7.23

--- src/main/java/com/oauth/user/UserInfoController.java
+++ src/main/java/com/oauth/user/UserInfoController.java
@@ -2,13 +2,17 @@ package com.oauth.user;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestMethod.*;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserInfoController {
 
-    @RequestMapping(method = RequestMethod.GET, name = "/getUserInfo")
-    public UserInfo getUserInfo() {
-        return new UserInfo("chenlizan", "123456");
+    @RequestMapping(value = "/getUserInfo", method = RequestMethod.GET)
+    @ResponseBody
+    public String getUserInfo() {
+        UserInfo userInfo = new UserInfo("chenlizan", "123456");
+        return userInfo.getUser();
     }
 }
--- src/test/java/com/oauth/user/UserInfoTests.java
+++ src/test/java/com/oauth/user/UserInfoTests.java
@@ -1,31 +1,47 @@
 package com.oauth.user;
 
 import com.oauth.main.OAuthApplication;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.net.URL;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = OAuthApplication.class,webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = OAuthApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserInfoTests {
+
+    @LocalServerPort
+    private int port;
+
+    private URL base;
 
     @Autowired
     private TestRestTemplate testRestTemplate;
     private UserInfo userInfo;
 
+    @Before
+    public void setUp() throws Exception {
+        String url = String.format("http://localhost:%d", port);
+        System.out.println(String.format("port is : [%d]", port));
+        this.base = new URL(url);
+    }
+
     @Test
     public void contextLoads() {
-        userInfo.setPassword("23456");
+        userInfo.setPassword("123456");
         userInfo.getPassword();
     }
 
     @Test
     public void getUserInfo() {
-
+        String userInfo = testRestTemplate.getForObject(this.base.toString() + "/getUserInfo", String.class);
+        System.out.println(userInfo);
     }
 
 }

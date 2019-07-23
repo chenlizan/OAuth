e61c2007d753686d442e091bd5889a4279c71f78
chenlizan <clz_318@live.com>
2019-07-24 06:42:28 +0800

    7.23

--- src/main/java/com/oauth/main/OAuthApplication.java
+++ src/main/java/com/oauth/main/OAuthApplication.java
@@ -2,8 +2,9 @@ package com.oauth.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.oauth")
 public class OAuthApplication {
 
     public static void main(String[] args) {

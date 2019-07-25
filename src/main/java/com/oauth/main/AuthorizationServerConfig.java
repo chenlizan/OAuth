97aaa96ed83667aa8ae11fee108c1f89a660071c
chenlizan <clz_318@live.com>
2019-07-25 22:14:53 +0800

    7.25

---
+++ src/main/java/com/oauth/main/AuthorizationServerConfig.java
@@ -0,0 +1,18 @@
+package com.oauth.main;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        return new JwtAccessTokenConverter();
+    }
+
+}
--- src/main/java/com/oauth/main/OAuthApplication.java
+++ src/main/java/com/oauth/main/OAuthApplication.java
@@ -2,7 +2,6 @@ package com.oauth.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = "com.oauth")
 public class OAuthApplication {

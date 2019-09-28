3a5ca3688f72cde6daeb5fe031d1d919f4bbab10
chenlizan <clz_318@live.com>
2019-09-28 20:44:08 +0800

    9.28

--- src/test/java/com/oauth/user/UserInfoTests.java
+++ src/test/java/com/oauth/user/UserInfoTests.java
@@ -13,10 +13,22 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.Serializable;
 import java.net.URL;
+
+class TestUserInfo implements Serializable {
+    private String username;
+    private String password;
+    private String[] roles;
+
+    public TestUserInfo(String username, String password, String[] roles) {
+        this.username = username;
+        this.password = password;
+        this.roles = roles;
+    }
+}
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = OAuthApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -39,12 +51,12 @@ public class UserInfoTests {
 
     @Test
     public void register() {
-        UserInfo userInfo = new UserInfo("chenlizan", "123456", AuthorityUtils.createAuthorityList("USER"));
+        TestUserInfo userInfo = new TestUserInfo("chenlizan", "123456", new String[]{"USER"});
         HttpHeaders headers = new HttpHeaders();
         MediaType type = MediaType.parseMediaType("application/json; charset=UTF-8");
         headers.setContentType(type);
-        headers.add("Accept", MediaType.APPLICATION_JSON.toString());
-        HttpEntity<UserInfo> httpEntity = new HttpEntity<UserInfo>(userInfo, headers);
+//        headers.add("Accept", MediaType.APPLICATION_JSON.toString());
+        HttpEntity<TestUserInfo> httpEntity = new HttpEntity<TestUserInfo>(userInfo, headers);
         ResponseEntity<UserInfo> responseEntity = testRestTemplate.postForEntity(this.base.toString() + "/register", httpEntity, UserInfo.class);
         System.out.println(responseEntity);
     }
